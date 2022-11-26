188ec2e13b8d993ea56ac8cabdd89b3ee4a663d8
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-11-26 11:32:42 +0300

    Добавьте файлы проекта.

---
+++ Restaurant/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Restaurant;
+
+WorkRestaurant workRestaurant = new();
+workRestaurant.Transformation();
+workRestaurant.Get();
+void Memu()
+{
+    Console.WriteLine($"Меню ресторана");
+    Console.WriteLine();
+    Console.WriteLine($"1. Burger");
+    Console.WriteLine($"2. Fries");
+    Console.WriteLine($"3. Chicken");
+    Console.WriteLine($"4. Pizza");
+    Console.WriteLine($"5. Sandwich");
+    Console.WriteLine($"6. Onionrings");
+    Console.WriteLine($"7. Milkshake");
+    Console.WriteLine($"8. Coke");
+    Console.WriteLine();
+}
---
+++ Restaurant/WorkRestaurant.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    internal class WorkRestaurant
+    {
+        private static string[] strings = new string[8] { "Burger", "Fries", "Chicken", "Pizza", "Sandwich", "Onionrings", "Milkshake", "Coke" };
+        private List<string> faw = new();
+        public void Transformation()
+        {
+            string enter = Console.ReadLine();            
+            foreach (string s in strings)
+            {
+                var trans= s.ToLower();
+                var a = enter.Contains(trans);                
+                if (a)
+                {
+                    faw.Add(s);
+                }
+            }
+        }
+        public void Get()
+        {
+            foreach(string s in faw)
+            {                
+                int g= 0;
+                foreach(var a in s)
+                {
+                    if (g < 1)
+                    {
+                        var c = char.ToUpper(a);
+                        Console.Write($"{c}");
+                    }
+                    else if (g >= 1)
+                    {
+                        Console.Write(a);
+                    }
+                    g++;                    
+                }
+                Console.Write(" ");
+            }
+        }
+    }
+}
